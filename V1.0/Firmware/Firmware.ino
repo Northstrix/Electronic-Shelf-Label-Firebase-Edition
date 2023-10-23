55e8517e7563935a063ee4baa73ae8f62ae0ea20
Maxim Bortnikov <43994704+Northstrix@users.noreply.github.com>
2023-10-23 19:47:42 +0700

    Add files via upload

---
+++ V1.0/Firmware/Firmware.ino
@@ -0,0 +1,439 @@
+/*
+Electronic Shelf Label Firebase Edition
+Distributed under the MIT License
+© Copyright Maxim Bortnikov 2023
+For more information please visit
+https://github.com/Northstrix/Electronic-Shelf-Label-Firebase-Edition
+https://sourceforge.net/projects/esl-firebase-edition/
+Required libraries:
+https://github.com/adafruit/Adafruit-ST7735-Library
+https://github.com/adafruit/Adafruit-GFX-Library
+https://github.com/adafruit/Adafruit_BusIO
+https://github.com/mobizt/Firebase-ESP-Client
+*/
+#include <Arduino.h>
+#if defined(ESP32) || defined(ARDUINO_RASPBERRY_PI_PICO_W)
+#include <WiFi.h>
+#elif defined(ESP8266)
+#include <ESP8266WiFi.h>
+#elif __has_include(<WiFiNINA.h>)
+#include <WiFiNINA.h>
+#elif __has_include(<WiFi101.h>)
+#include <WiFi101.h>
+#elif __has_include(<WiFiS3.h>)
+#include <WiFiS3.h>
+#endif
+#include <Firebase_ESP_Client.h>
+#include <addons/TokenHelper.h>
+#include <addons/RTDBHelper.h>
+#include "DES.h"
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_ST7735.h>
+#define TFT_CS         D2
+#define TFT_RST        D3
+#define TFT_DC         D4
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
+DES des;
+
+#define WIFI_SSID "accessPpointName"
+#define WIFI_PASSWORD "accessPointPassword"
+#define API_KEY "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+#define DATABASE_URL "https://database-name-default-rtdb.firebaseio.com/"
+
+byte des_key[] = {
+0x3a,0xab,0x66,0x62,0xde,0xff,0x21,0xd3,
+0x64,0xed,0xc6,0x67,0xa8,0xe0,0xbb,0xcf,
+0x0d,0xac,0xaa,0xc3,0x25,0xdc,0xb2,0xb5
+};
+
+String id = "Abcde";
+int delay_in_seconds = 600; // request data every 10 minutes
+
+FirebaseData fbdo;
+FirebaseAuth auth;
+FirebaseConfig config;
+
+unsigned long dataMillis = 0;
+int count = 0;
+bool signupOK = false;
+
+String dec_st;
+
+#if defined(ARDUINO_RASPBERRY_PI_PICO_W)
+WiFiMulti multi;
+#endif
+
+void read_file(String filename, int y) {
+  back_k();
+  dec_st = "";
+  if(Firebase.RTDB.getString(&fbdo, filename.c_str())){
+    String ct = fbdo.to<String>();
+    int ct_len1 = ct.length() + 1;
+    char ct_array1[ct_len1];
+    ct.toCharArray(ct_array1, ct_len1);
+    int ext = 0;
+    while (ct_len1 > ext) {
+      split_for_dec_des_only(ct_array1, ct_len1, 0 + ext);
+      ext += 16;
+    }
+    disp_centered_text(dec_st, y);
+  }
+  rest_k();
+}
+
+void disp_centered_text(String t_disp, int y){
+  if (t_disp.length() < 27){
+    int16_t x1, y1;
+    uint16_t w, h;
+    tft.getTextBounds(t_disp, 160, 0, &x1, &y1, &w, &h);
+    tft.setCursor(80 - (w / 2), y);
+    tft.print(t_disp);
+  }
+  else{
+    tft.setCursor(0, y);
+    for (int i = 0; i < 26; i++){
+      tft.print(t_disp.charAt(i));
+    }
+  }
+}
+
+uint8_t Inv_S_Box[16][16] = {  
+    {0x52, 0x09, 0x6A, 0xD5, 0x30, 0x36, 0xA5, 0x38, 0xBF, 0x40, 0xA3, 0x9E, 0x81, 0xF3, 0xD7, 0xFB},  
+    {0x7C, 0xE3, 0x39, 0x82, 0x9B, 0x2F, 0xFF, 0x87, 0x34, 0x8E, 0x43, 0x44, 0xC4, 0xDE, 0xE9, 0xCB},  
+    {0x54, 0x7B, 0x94, 0x32, 0xA6, 0xC2, 0x23, 0x3D, 0xEE, 0x4C, 0x95, 0x0B, 0x42, 0xFA, 0xC3, 0x4E},  
+    {0x08, 0x2E, 0xA1, 0x66, 0x28, 0xD9, 0x24, 0xB2, 0x76, 0x5B, 0xA2, 0x49, 0x6D, 0x8B, 0xD1, 0x25},  
+    {0x72, 0xF8, 0xF6, 0x64, 0x86, 0x68, 0x98, 0x16, 0xD4, 0xA4, 0x5C, 0xCC, 0x5D, 0x65, 0xB6, 0x92},  
+    {0x6C, 0x70, 0x48, 0x50, 0xFD, 0xED, 0xB9, 0xDA, 0x5E, 0x15, 0x46, 0x57, 0xA7, 0x8D, 0x9D, 0x84},  
+    {0x90, 0xD8, 0xAB, 0x00, 0x8C, 0xBC, 0xD3, 0x0A, 0xF7, 0xE4, 0x58, 0x05, 0xB8, 0xB3, 0x45, 0x06},  
+    {0xD0, 0x2C, 0x1E, 0x8F, 0xCA, 0x3F, 0x0F, 0x02, 0xC1, 0xAF, 0xBD, 0x03, 0x01, 0x13, 0x8A, 0x6B},  
+    {0x3A, 0x91, 0x11, 0x41, 0x4F, 0x67, 0xDC, 0xEA, 0x97, 0xF2, 0xCF, 0xCE, 0xF0, 0xB4, 0xE6, 0x73},  
+    {0x96, 0xAC, 0x74, 0x22, 0xE7, 0xAD, 0x35, 0x85, 0xE2, 0xF9, 0x37, 0xE8, 0x1C, 0x75, 0xDF, 0x6E},  
+    {0x47, 0xF1, 0x1A, 0x71, 0x1D, 0x29, 0xC5, 0x89, 0x6F, 0xB7, 0x62, 0x0E, 0xAA, 0x18, 0xBE, 0x1B},  
+    {0xFC, 0x56, 0x3E, 0x4B, 0xC6, 0xD2, 0x79, 0x20, 0x9A, 0xDB, 0xC0, 0xFE, 0x78, 0xCD, 0x5A, 0xF4},  
+    {0x1F, 0xDD, 0xA8, 0x33, 0x88, 0x07, 0xC7, 0x31, 0xB1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xEC, 0x5F},  
+    {0x60, 0x51, 0x7F, 0xA9, 0x19, 0xB5, 0x4A, 0x0D, 0x2D, 0xE5, 0x7A, 0x9F, 0x93, 0xC9, 0x9C, 0xEF},  
+    {0xA0, 0xE0, 0x3B, 0x4D, 0xAE, 0x2A, 0xF5, 0xB0, 0xC8, 0xEB, 0xBB, 0x3C, 0x83, 0x53, 0x99, 0x61},  
+    {0x17, 0x2B, 0x04, 0x7E, 0xBA, 0x77, 0xD6, 0x26, 0xE1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0C, 0x7D}  
+};
+
+byte back_des_key[24];
+
+void back_k() {
+  for (int i = 0; i < 24; i++) {
+    back_des_key[i] = des_key[i];
+  }
+}
+
+void rest_k() {
+  for (int i = 0; i < 24; i++) {
+    des_key[i] = back_des_key[i];
+  }
+}
+
+int getNum(char ch)
+{
+    int num=0;
+    if(ch>='0' && ch<='9')
+    {
+        num=ch-0x30;
+    }
+    else
+    {
+        switch(ch)
+        {
+            case 'A': case 'a': num=10; break;
+            case 'B': case 'b': num=11; break;
+            case 'C': case 'c': num=12; break;
+            case 'D': case 'd': num=13; break;
+            case 'E': case 'e': num=14; break;
+            case 'F': case 'f': num=15; break;
+            default: num=0;
+        }
+    }
+    return num;
+}
+
+void incr_des_key() {
+  if (des_key[7] == 255) {
+    des_key[7] = 0;
+    if (des_key[6] == 255) {
+      des_key[6] = 0;
+      if (des_key[5] == 255) {
+        des_key[5] = 0;
+        if (des_key[4] == 255) {
+          des_key[4] = 0;
+          if (des_key[3] == 255) {
+            des_key[3] = 0;
+            if (des_key[2] == 255) {
+              des_key[2] = 0;
+              if (des_key[1] == 255) {
+                des_key[1] = 0;
+                if (des_key[0] == 255) {
+                  des_key[0] = 0;
+                } else {
+                  des_key[0]++;
+                }
+              } else {
+                des_key[1]++;
+              }
+            } else {
+              des_key[2]++;
+            }
+          } else {
+            des_key[3]++;
+          }
+        } else {
+          des_key[4]++;
+        }
+      } else {
+        des_key[5]++;
+      }
+    } else {
+      des_key[6]++;
+    }
+  } else {
+    des_key[7]++;
+  }
+
+  if (des_key[15] == 255) {
+    des_key[15] = 0;
+    if (des_key[14] == 255) {
+      des_key[14] = 0;
+      if (des_key[13] == 255) {
+        des_key[13] = 0;
+        if (des_key[12] == 255) {
+          des_key[12] = 0;
+          if (des_key[11] == 255) {
+            des_key[11] = 0;
+            if (des_key[10] == 255) {
+              des_key[10] = 0;
+              if (des_key[9] == 255) {
+                des_key[9] = 0;
+                if (des_key[8] == 255) {
+                  des_key[8] = 0;
+                } else {
+                  des_key[8]++;
+                }
+              } else {
+                des_key[9]++;
+              }
+            } else {
+              des_key[10]++;
+            }
+          } else {
+            des_key[11]++;
+          }
+        } else {
+          des_key[12]++;
+        }
+      } else {
+        des_key[13]++;
+      }
+    } else {
+      des_key[14]++;
+    }
+  } else {
+    des_key[15]++;
+  }
+
+  if (des_key[23] == 255) {
+    des_key[23] = 0;
+    if (des_key[22] == 255) {
+      des_key[22] = 0;
+      if (des_key[21] == 255) {
+        des_key[21] = 0;
+        if (des_key[20] == 255) {
+          des_key[20] = 0;
+          if (des_key[19] == 255) {
+            des_key[19] = 0;
+            if (des_key[18] == 255) {
+              des_key[18] = 0;
+              if (des_key[17] == 255) {
+                des_key[17] = 0;
+                if (des_key[16] == 255) {
+                  des_key[16] = 0;
+                } else {
+                  des_key[16]++;
+                }
+              } else {
+                des_key[17]++;
+              }
+            } else {
+              des_key[18]++;
+            }
+          } else {
+            des_key[19]++;
+          }
+        } else {
+          des_key[20]++;
+        }
+      } else {
+        des_key[21]++;
+      }
+    } else {
+      des_key[22]++;
+    }
+  } else {
+    des_key[23]++;
+  }
+}
+
+void split_for_dec_des_only(char ct[], int ct_len, int p){
+  int br = false;
+  byte res[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+  for (int i = 0; i < 16; i+=2){
+    if(i+p > ct_len - 1){
+      br = true;
+      break;
+    }
+    if (i == 0){
+    if(ct[i+p] != 0 && ct[i+p+1] != 0)
+    res[i] = 16*getNum(ct[i+p])+getNum(ct[i+p+1]);
+    if(ct[i+p] != 0 && ct[i+p+1] == 0)
+    res[i] = 16*getNum(ct[i+p]);
+    if(ct[i+p] == 0 && ct[i+p+1] != 0)
+    res[i] = getNum(ct[i+p+1]);
+    if(ct[i+p] == 0 && ct[i+p+1] == 0)
+    res[i] = 0;
+    }
+    else{
+    if(ct[i+p] != 0 && ct[i+p+1] != 0)
+    res[i/2] = 16*getNum(ct[i+p])+getNum(ct[i+p+1]);
+    if(ct[i+p] != 0 && ct[i+p+1] == 0)
+    res[i/2] = 16*getNum(ct[i+p]);
+    if(ct[i+p] == 0 && ct[i+p+1] != 0)
+    res[i/2] = getNum(ct[i+p+1]);
+    if(ct[i+p] == 0 && ct[i+p+1] == 0)
+    res[i/2] = 0;
+    }
+  }
+    if(br == false){
+      byte out[8];
+      des.tripleDecrypt(out, res, des_key);
+      incr_des_key();
+      Inverse_S_Box(out);
+  }
+}
+
+void Inverse_S_Box(byte decr_with_des[8]){
+  byte aft_inv_box[8];
+  for (int count; count<4; count++){
+  String strOne = "";
+  String strTwo = "";
+  int i = count * 2;
+  int j = count * 2 + 1;
+  int fir = decr_with_des[i];
+  int sec = decr_with_des[j];
+  if (fir < 16)
+  strOne += 0;
+  strOne +=  String(fir, HEX);
+  if (sec < 16)
+  strTwo += 0;
+  strTwo +=  String(sec, HEX);  
+
+  char chars_f[3];
+  char chars_s[3];
+  strOne.toCharArray(chars_f, 3);
+  strTwo.toCharArray(chars_s, 3);
+  chars_f[2] = '\0';
+  chars_s[2] = '\0';
+
+  int flc = getNum(chars_f[0]);
+  int frc = getNum(chars_f[1]);
+  int slc = getNum(chars_s[0]);
+  int src = getNum(chars_s[1]);
+
+  aft_inv_box[i] = ("%x",Inv_S_Box[flc][frc]);
+  aft_inv_box[j] = ("%x",Inv_S_Box[slc][src]);
+  }
+
+  for (int i = 0; i < 8; i++){
+    char x = aft_inv_box[i];
+    if (x > 0)
+      dec_st += char(x); 
+  }
+}
+
+void setup()
+{
+  tft.initR(INITR_BLACKTAB);
+  tft.setRotation(1);
+  tft.fillScreen(0x0000);
+  tft.setTextSize(1);
+  tft.setTextColor(0xffff);
+  tft.setCursor(0, 5);
+  tft.println("Connecting to Wi-Fi");
+
+#if defined(ARDUINO_RASPBERRY_PI_PICO_W)
+    multi.addAP(WIFI_SSID, WIFI_PASSWORD);
+    multi.run();
+#else
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+#endif
+   tft.setCursor(0, 20);
+    //Serial.print("Connecting to Wi-Fi");
+    unsigned long ms = millis();
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(300);
+        tft.print("#");
+    }
+    //Serial.println();
+    //Serial.print("Connected with IP: ");
+    //Serial.println(WiFi.localIP());
+    //Serial.println();
+
+    //Serial.printf("Firebase Client v%s\n\n", FIREBASE_CLIENT_VERSION);
+    config.api_key = API_KEY;
+    config.database_url = DATABASE_URL;
+#if defined(ARDUINO_RASPBERRY_PI_PICO_W)
+    config.wifi.clearAP();
+    config.wifi.addAP(WIFI_SSID, WIFI_PASSWORD);
+#endif
+
+    // Comment or pass false value when WiFi reconnection will control by your code or third party library e.g. WiFiManager
+    Firebase.reconnectNetwork(true);
+
+    fbdo.setBSSLBufferSize(4096 /* Rx buffer size in bytes from 512 - 16384 */, 1024 /* Tx buffer size in bytes from 512 - 16384 */);
+
+    tft.fillScreen(0x0000);
+    disp_centered_text("Signing Up to Firebase", 10);
+
+    if (Firebase.signUp(&config, &auth, "", ""))
+    {
+        //Serial.println("ok");
+        signupOK = true;
+    }
+    else
+        //Serial.printf("%s\n", config.signer.signupError.message.c_str());
+
+    config.token_status_callback = tokenStatusCallback; // see addons/TokenHelper.h
+    Firebase.begin(&config, &auth);
+    tft.setCursor(0, 5);
+    tft.println("Retrieving Data...");
+    retrieve_data_from_the_cloud();
+}
+
+void retrieve_data_from_the_cloud(){
+  tft.fillScreen(0x0000);
+  tft.setTextSize(1);
+  read_file(id + "_line1", 5);
+  read_file(id + "_line2", 20);
+  read_file(id + "_line3", 35);
+  read_file(id + "_line4", 50);
+  read_file(id + "_line5", 65);
+  read_file(id + "_line6", 80);
+  read_file(id + "_line7", 95);
+  tft.setTextSize(2);
+  read_file(id + "_price", 110);
+}
+
+void loop()
+{
+    if (millis() - dataMillis > (1000 * delay_in_seconds) && signupOK && Firebase.ready())
+    {
+        dataMillis = millis();
+        retrieve_data_from_the_cloud();
+    }
+}
